e36c6d3ed046f2986abf59efbb9c537c307dc722
nc <neilchaudhary12@gmail.com>
2025-11-19 23:15:39 +0530

    feat: updated mongoDB connect

--- internal/storage/mongoDB.go
+++ internal/storage/mongoDB.go
@@ -9,40 +9,54 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.uber.org/zap"
 )
 
+var (
+	mongoClient *mongo.Client
+	log         *zap.SugaredLogger
+)
+
+func init() {
+	log = logger.GetLogger("internal/storage/mongoDB")
+}
+
 func ConnectMongoDB() {
-
-	log := logger.GetLogger("internal/storage/mongoDB")
-
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Warning: .env file not found or could not be loaded")
+		log.Warn("Warning: .env file not found or could not be loaded")
 	}
 
 	var mongoURI string
-
 	if mongoURI = os.Getenv("mongoURI"); mongoURI == "" {
 		log.Fatal("You must set your 'mongoURI' environment variable.")
 	}
 
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().
+		ApplyURI(mongoURI).
+		SetMaxPoolSize(50).
+		SetMinPoolSize(10).
+		SetServerAPIOptions(serverAPI)
 
-	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
-
-	mongoClient, err := mongo.Connect(context.TODO(), opts)
+	var err error
+	mongoClient, err = mongo.Connect(context.TODO(), opts)
 	if err != nil {
-		panic(err)
+		log.Fatal("Error connecting to MongoDB:", err)
 	}
-	defer func() {
-		if err = mongoClient.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
 
+	// Ping to verify connection
 	var result bson.M
 	if err := mongoClient.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
-		panic(err)
+		log.Fatal("Could not ping MongoDB:", err)
 	}
 	log.Info("Pinged your deployment. You successfully connected to MongoDB!")
+}
 
+func GetDatabase(dbName string) *mongo.Database {
+	if mongoClient == nil {
+		log.Info("MongoDB client is not initialized. Please call ConnectMongoDB first.")
+
+	}
+	ConnectMongoDB()
+	return mongoClient.Database(dbName)
 }
