a73515540b184cc3671ccf0dff0b3556ac364288
neel c <neilchaudhary12@gmail.com>
2025-11-28 18:08:52 +0530

    feat: edits

--- internal/core/services/data.go
+++ internal/core/services/data.go
@@ -1,5 +1,7 @@
 package core
 
+import "go.mongodb.org/mongo-driver/mongo"
+
 // will try to maximise its utility across the codebase, hence using any as data type
 func BatchData[T any](data []T, size int) []T {
 	batch := make([]T, size)
@@ -8,4 +10,8 @@ func BatchData[T any](data []T, size int) []T {
 	}
 
 	return batch
+}
+
+func FetchDataFromDB(collectionName string) (*mongo.Collection, error) {
+
 }
