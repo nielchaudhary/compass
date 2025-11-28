52cd4b684d66de4fd96ed273b2e4229c1334963d
neel c <neilchaudhary12@gmail.com>
2025-11-28 16:50:13 +0530

    feat: added scheduler

--- internal/monitor/services/scheduler.go
+++ internal/monitor/services/scheduler.go
@@ -1,1 +1,43 @@
 package monitor
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	gocron "github.com/go-co-op/gocron/v2"
+	logService "github.com/nielchaudhary/compass/pkg/logger"
+)
+
+func SchedulerCore() {
+
+	logger := logService.GetLogger()
+
+	defer func() {
+		if err := logger.Sync(); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to sync logger: %v\n", err)
+		}
+	}()
+
+	scheduler, newSchedulerError := gocron.NewScheduler()
+	if newSchedulerError != nil {
+		log.Fatal("Error Initialising the core scheduler", newSchedulerError)
+	}
+
+	repeatInterval := gocron.DurationJob(1 * time.Second)
+	healthCheckTask := gocron.NewTask(func() {
+		_, err := PingEndpoint("http://localhost:3000/health", "GET")
+		if err != nil {
+			logger.Error("Error pinging endpoint")
+		}
+	})
+
+	_, newJobError := scheduler.NewJob(repeatInterval, healthCheckTask)
+	if newJobError != nil {
+		log.Fatal("Error running the cron job ", newJobError)
+	}
+
+	scheduler.Start()
+
+}
