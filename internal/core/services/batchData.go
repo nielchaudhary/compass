84eda6f267a8424f1ba9d910c538318f5a08e5fc
neel c <neilchaudhary12@gmail.com>
2025-11-28 17:25:21 +0530

    feat: batching with generic sizing, inputs

--- internal/core/services/batchData.go
+++ internal/core/services/batchData.go
@@ -1,9 +1,9 @@
 package core
 
 // will try to maximise its utility across the codebase, hence using any as data type
-func BatchData[T any](data []T) [20]T {
-	var batch [20]T
-	for i := 0; i < len(data) && i < 20; i++ {
+func BatchData[T any](data []T, size int) []T {
+	batch := make([]T, size)
+	for i := 0; i < len(data) && i < size; i++ {
 		batch[i] = data[i]
 	}
 
