5954348904b96ef4d55f123426b41b7d5c2fc4f4
neel c <neilchaudhary12@gmail.com>
2025-12-07 18:30:34 +0530

    feat: added uptime scheduler

--- internal/scheduler/uptime-scheduler.go
+++ internal/scheduler/uptime-scheduler.go
@@ -6,16 +6,39 @@ import (
 	"github.com/nielchaudhary/compass/pkg/logger"
 )
 
-func UptimeScheduler() ([]core.Endpoints, error) {
+type UptimeSchedulerResp struct {
+	Health string `bson:"health"`
+	Error  string `bson:"error"`
+}
 
+func UptimeScheduler() ([]UptimeSchedulerResp, error) {
 	log := logger.GetLogger()
 	defer log.Sync()
 
 	endpoints, fetchEndpointFromDBError := core.FetchEndpointsDataFromDB("endpoints")
-
 	if fetchEndpointFromDBError != nil {
-		log.Error("Could not fetch endpoints from DB due to : ", fetchEndpointFromDBError)
+		log.Error("Could not fetch endpoints from DB due to: ", fetchEndpointFromDBError)
 		return nil, fetchEndpointFromDBError
 	}
 
+	var uptimeResults []UptimeSchedulerResp
+
+	for i := 0; i < len(endpoints); i++ {
+		pingServiceResp, pingServiceError := PingEndpoint(endpoints[i].Endpoint, endpoints[i].Method)
+
+		if pingServiceError != nil {
+			uptimeResults = append(uptimeResults, UptimeSchedulerResp{
+				Health: "",
+				Error:  pingServiceError.Error(),
+			})
+			continue
+		}
+
+		uptimeResults = append(uptimeResults, UptimeSchedulerResp{
+			Health: pingServiceResp.ServiceStatus,
+			Error:  "",
+		})
+	}
+
+	return uptimeResults, nil
 }
