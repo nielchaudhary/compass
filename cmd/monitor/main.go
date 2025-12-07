2a9596772459462990bed21829997f619ae96d1e
neel c <neilchaudhary12@gmail.com>
2025-12-07 13:57:30 +0530

    feat: fixed imports

--- cmd/monitor/main.go
+++ cmd/monitor/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/nielchaudhary/compass/internal/config"
 	monitor "github.com/nielchaudhary/compass/internal/monitor/router"
-	scheduler "github.com/nielchaudhary/compass/internal/monitor/scheduler"
+	scheduler "github.com/nielchaudhary/compass/internal/scheduler"
 	storage "github.com/nielchaudhary/compass/internal/storage"
 	constants "github.com/nielchaudhary/compass/pkg/constants"
 	logger "github.com/nielchaudhary/compass/pkg/logger"
